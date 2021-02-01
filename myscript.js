d172f6b8328e2b5eeaccf949f22728f0d407f226
Azuthedog1 <sb.and.bigsur@gmail.com>
2021-02-01 10:19:41 -0800

    added questions and answers

--- myscript.js
+++ myscript.js
@@ -1,6 +1,51 @@
 $(document).ready(function() {
-	$("#Q1").click(function() {
+	$("#q1").click(function() {
 
-		$("#Q1A").toggle();
+		$("#q1a").toggle();
+	});
+
+	$("#q2").click(function() {
+
+		$("#q2a").toggle();
+	});
+
+	$("#q3").click(function() {
+
+		$("#q3a").toggle();
+	});
+
+	$("#q4").click(function() {
+
+		$("#q4a").toggle();
+	});
+
+	$("#q5").click(function() {
+
+		$("#q5a").toggle();
+	});
+
+	$("#q6").click(function() {
+
+		$("#q6a").toggle();
+	});
+
+	$("#q7").click(function() {
+
+		$("#q7a").toggle();
+	});
+
+	$("#q8").click(function() {
+
+		$("#q8a").toggle();
+	});
+
+	$("#q9").click(function() {
+
+		$("#q9a").toggle();
+	});
+
+	$("#q10").click(function() {
+
+		$("#q10a").toggle();
 	});
 });
