63e485710db5919cec5d5ff523502d22ddbca396
Azuthedog1 <sb.and.bigsur@gmail.com>
2021-02-02 10:15:38 -0800

    added loop and styling

--- myscript.js
+++ myscript.js
@@ -1,51 +1,11 @@
 $(document).ready(function() {
-	$("#q1").click(function() {
+	var q;
+	for(q=1; q < 10; q++){
 
-		$("#q1a").toggle();
+	$("#q" + q).click({num:q},function(x) {
+		$("#a" + x.data.num).toggle();
 	});
 
-	$("#q2").click(function() {
-
-		$("#q2a").toggle();
-	});
-
-	$("#q3").click(function() {
-
-		$("#q3a").toggle();
-	});
-
-	$("#q4").click(function() {
-
-		$("#q4a").toggle();
-	});
-
-	$("#q5").click(function() {
-
-		$("#q5a").toggle();
-	});
-
-	$("#q6").click(function() {
-
-		$("#q6a").toggle();
-	});
-
-	$("#q7").click(function() {
-
-		$("#q7a").toggle();
-	});
-
-	$("#q8").click(function() {
-
-		$("#q8a").toggle();
-	});
-
-	$("#q9").click(function() {
-
-		$("#q9a").toggle();
-	});
-
-	$("#q10").click(function() {
-
-		$("#q10a").toggle();
-	});
+	
+	}
 });
